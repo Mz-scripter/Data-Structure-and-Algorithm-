0f3cac026f9d387d7f22e477977c2806935d8b1c
Adeko Muheez <161574712+Mzed-io@users.noreply.github.com>
2024-09-02 15:39:51 +0100

    Create 006_summary_ranges.py

---
+++ 006_summary_ranges.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+  def summaryRanges(self, nums: List[int]) -> List[str]:
+    ans = []
+    i = 0
+    while i < len(nums):
+      start = nums[i]
+      while i < len(nums) - 1 and nums[i] + 1 == nums[i + 1]:
+        i += 1
+      if start != nums[i]:
+        ans.append(f"{start}->{nums[i]}")
+      else:
+        ans.append(f"{start}")
+      i += 1
+    return ans
