cf669d3faa40c7424b5e58447d48e113137bf0ba
Adeko Muheez <161574712+Mzed-io@users.noreply.github.com>
2024-09-09 06:42:42 +0100

    Update 008_merged_intervals_revised.py

--- 008_merged_intervals_revised.py
+++ 008_merged_intervals_revised.py
@@ -10,18 +10,9 @@ class Solution:
         for interval in intervals:
             if not merged or merged[-1][1] < interval[0]:
                 merged.append(interval)
-                # The if condition checks if the merged list is empty or if the 2nd number in the last
-                # interval of merged list is less than the starting number in the interval list
             else:
                 merged[-1] = [merged[-1][0], max(merged[-1][1], interval[1])]
-        #         If there is overlap, the last list in the merged will be replaced by the starting
-        #         of the merged list since that is the smallest, and the ending will be replaced by
-        #         the maximum of the second number in the merged list and interval
         return merged
-
-
+        
 #     Time complexity: O(n logn)
 #     Space Complexity: O(n)
-
-result = Solution()
-print(result.merge([[1,4],[4,5]]))
